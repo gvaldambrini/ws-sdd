be6f82a64fcf08c7768ef9c815375311d15d2baf
Gianni <aleister@develer.com>
2019-06-27 17:25:01 +0200

    add storybook addons and update package.json

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,19 @@
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+import { withA11y } from '@storybook/addon-a11y';
+import { addDecorator, addParameters } from '@storybook/react';
 
-const req = require.context('../src/components', true, /\.stories\.js$/);
+addDecorator(withA11y);
+
+addParameters({
+  backgrounds: [
+    { name: 'transparent', value: 'transparent', default: true },
+    { name: 'twitter', value: '#00aced'},
+    { name: 'facebook', value: '#3b5998' },
+  ],
+});
+
+const req = requireContext('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
