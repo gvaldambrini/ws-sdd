8c186a2ddde487f85f3327615419d5d0618a13f2
Gianni <aleister@develer.com>
2019-06-28 16:57:53 +0200

    remove boilerplate example code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Link } from "react-router-dom";
-
 import "./App.scss";
 
-const App = props => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
-    <hr />
-    {props.children}
-  </div>
-);
+const App = props => <div>{props.children}</div>;
 
 App.propTypes = {
   children: PropTypes.any
--- src/index.js
+++ src/index.js
@@ -6,21 +6,16 @@ import { Provider } from "react-redux";
 
 import thunkMiddleware from "redux-thunk";
 
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import rootReducer from "./modules";
 import App from "./components/App";
-import Home from "./containers/Home";
-import About from "./containers/About";
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 const routes = (
   <Router>
-    <App>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </App>
+    <App>Empty App</App>
   </Router>
 );
 
